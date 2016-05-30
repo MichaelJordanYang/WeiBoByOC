4881977c66a456c414d69d197e7ca72eda676eb6
jordanYang <897733537@qq.com>
2016-05-30 08:41:29 +0800

    自定义搜索框
    自定义搜索框

--- WeiBoByOC/WeiBoByOC/Classes/Discover/Controller/JDYDiscoverViewController.m
+++ WeiBoByOC/WeiBoByOC/Classes/Discover/Controller/JDYDiscoverViewController.m
@@ -23,6 +23,8 @@
 //    
 //    self.navigationItem.titleView = searchBar;
     
+    
+    
     //创建搜索框对象
     UITextField *searchBar = [[UITextField alloc] init];
     searchBar.JDY_width = 357;
@@ -32,13 +34,20 @@
     searchBar.background = [UIImage imageNamed:@"searchbox"];
     
     //设置左边的放大镜图标
-    UIImageView *searchBarIcon = [[UIImageView alloc] init];
-    searchBarIcon.image = [UIImage imageNamed:@"sousuo"];
+//    UIImageView *searchBarIcon = [[UIImageView alloc] init];
+//    searchBarIcon.image = [UIImage imageNamed:@"sousuo"];
+    UIImage *image = [UIImage imageNamed:@"sousuo"];
+    UIImageView *searchBarIcon = [[UIImageView alloc] initWithImage:image];
     searchBarIcon.JDY_width = 30;
     searchBarIcon.JDY_height = 30;
+    
+//    searchBarIcon.backgroundColor = [UIColor orangeColor];
+//    //设置搜索内容保持中心不变
     searchBarIcon.contentMode = UIViewContentModeCenter;
 
+    //设置左边的放大镜view的内容
     searchBar.leftView = searchBarIcon;
+    //设置左边的view什么时候显示.=总是显示
     searchBar.leftViewMode = UITextFieldViewModeAlways;
     
     self.navigationItem.titleView = searchBar;
